35ef7265c60d79419bc8b6737f1f133df810b4da
Jacob Garcia <janikgar@gmail.com>
2018-12-07 21:48:06 -0600

    build script + colors

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as program from 'commander';
+import chalk from 'chalk';
+
+program
+  .version('0.0.1')
+  .option('-d, --today', 'Get today\'s weather')
+  .option('-w, --week', 'Get this week\s weather')
+  .parse(process.argv)
+
+console.log(chalk.blue('You\'ve asked for the weather for:'))
+if (program.today) {
+  console.log(chalk.yellowBright(" - Today"));
+}
+if (program.week) {
+  console.log(chalk.green(" - This week"));
+}
